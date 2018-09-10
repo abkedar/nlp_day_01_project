ae01a3bb0f084c5da0c0ab1326be4176e2185333
abkedar <anupbhande@gmail.com>
2018-09-10 18:19:58 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,6 +1,21 @@
+# %load q01_load_data/build.py
 # Default imports
+import pandas as pd
 from sklearn.datasets import load_files
+
+
 from sklearn.model_selection import train_test_split
 
-path = 'data/20news-bydate-train/'
+path_1 = 'data/20news-bydate-train/'
 # Your solution here:
+def q01_load_data(path, seed=9):
+    
+    #'write your solution here'
+
+    categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+    twenty_train = load_files(path, categories)
+    X_train, X_test, y_train, y_test = train_test_split(twenty_train.data, twenty_train.target, test_size=0.8,
+                                                        random_state=seed)
+    return twenty_train, X_train, X_test, y_train, y_test
+
+
