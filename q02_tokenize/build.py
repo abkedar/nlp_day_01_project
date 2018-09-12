6e91f5a320e713a94700627b9a16af8a2fe9415b
abkedar <anupbhande@gmail.com>
2018-09-12 02:27:45 +0000

    Done

--- q02_tokenize/build.py
+++ q02_tokenize/build.py
@@ -1,3 +1,4 @@
+# %load q02_tokenize/build.py
 # Default imports
 
 from nltk.tokenize import TreebankWordTokenizer
@@ -7,3 +8,17 @@ import pandas as pd
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
 
 # Write your solution here:
+path = 'data/20news-bydate-train/'
+
+def q02_tokenize(data):
+    
+    'write your solution here'
+    twenty_train, X_train, X_test, y_train, y_test = q01_load_data(path)
+    tokenizer = TreebankWordTokenizer()
+    to_tokenize = pd.Series(X_train)
+    almost_tokenized = to_tokenize.apply(lambda row: row.lower())
+    tokenized = almost_tokenized.apply(lambda row: tokenizer.tokenize(str(row)))
+    # X_train.head(20)
+    return tokenized
+q02_tokenize(path)
+
